591c95cad724c01c061a6aa0903c022088b27423
seongaelee <salee@yujinrobot.com>
2019-11-01 16:48:13 +0900

    #충돌

--- Project1/View/IntroViewController.swift
+++ Project1/View/IntroViewController.swift
@@ -34,7 +34,6 @@ class IntroViewController: UIViewController, TitleStackViewDataSource {
         if #available(iOS 13.0, *) {
             overrideUserInterfaceStyle = .light
         }
-//        self.imageData = ["수빈.png","이삭.png","다은.png","아형.png"]
         
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
@@ -92,20 +91,18 @@ class IntroViewController: UIViewController, TitleStackViewDataSource {
         
     }
     
-//    @IBAction func albumClicked(_ sender: UIButton) {
-//        clickedInHome(photoButton)
-//    }
-    
-    
-//    @IBAction func scheduleClicked(_ sender: UIButton) {
-//        clickedInHome(scheduleButton)
-//    }
-    
-    
     @objc func clickedInHome(_ sender: UIImageView) {
         let btnTitle = "Album"
         
         self.revealViewController()?.rearViewController.performSegue(withIdentifier: btnTitle, sender: self.revealViewController()?.rearViewController)
+    }
+    
+    @IBAction func notifyButton(_ sender: Any) {
+        let alert = UIAlertController(title: "준비중입니다", message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
@@ -151,6 +148,9 @@ extension IntroViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0.0
     }
+    
+    
+    
 }
 
 
